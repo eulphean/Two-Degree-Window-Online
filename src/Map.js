4cbd5339ddde2a368ca2f6eecc4a5080f86df64a
Amay Kataria <amkatari@microsoft.com>
2019-10-29 15:00:43 -0500

    Commit cleanup changes. Now adding tooltip to the paths

--- src/Map.js
+++ src/Map.js
@@ -1,40 +1,55 @@
 // A class to create a Map. 
 class Map {
     constructor(width, height) {
+        this.width = width; this.height = height;
+
         // Empty svg as the root.
-        var svg = d3.select(".map").append("svg")
+        this.svg = d3.select(".map").append("svg")
                 .attr('preserveAspectRadio', 'none')
                 .attr('viewBox', [0, 0, width, height])
-        console.log(svg);
         
         // Load TopoJson file and create the map. 
-        this.loadMap(svg, width, height); 
+        this.initMap(); 
     }
 
-    loadMap(svg, width, height) {
-        d3.json("timezones.json").then((topology) => { 
-            console.log(topology);
+    initMap() {
+        d3.json(timezoneFile).then((topology) => { 
+            // console.log(topology);
 
             // Extract all the feature data into a GeoJson object. 
             var geojson = topojson.feature(topology, topology.objects.timezone_oceans_geo);
-            console.log(geojson);
+            // console.log(geojson);
         
             // Create Projection (how lat/long will be converted to 2D pixel space). 
             // Do scaling to fit the map on the screen. 
             var geoProjection = d3.geoEquirectangular(); 
-            geoProjection.fitSize([width, height], geojson);
+            geoProjection.fitSize([this.width, this.height], geojson);
         
             // Geo shape object that converts the actual geometry into shapes to be drawn. 
             var geoPath = d3.geoPath(geoProjection);
         
-            // Join FeatureCollection's features to path elements.
-            // Bind date
-            // Append path elements 'd' attribute to the geoPath (path string that defines the svg path) 
-            svg.selectAll("path")
-            .data(geojson.features)
-            .enter().append("path")
-            .attr("d", geoPath);
+            // Bind geogson features to path elements and then append (enter) new path element. 
+            // Append path elements 'd' attribute to the geoPath (path string that defines the svg path)
+            this.svg.selectAll("path")
+                .data(geojson.features)
+                .enter().append("path")
+                .attr("d", geoPath)
+                .on("mouseover", this.handleMouseover)
+                .on("mouseout", this.handleMouseout);
         });
+    }
+
+    handleMouseover(d, i) {
+        console.log(d.properties.tzid); // Get a text element 
+        d3.select(d3.event.target)
+            .transition()
+            .style('fill', 'black');
+    }
+
+    handleMouseout(d, i) {
+        d3.select(d3.event.target)
+        .transition()
+        .style('fill', 'white');
     }
 }
 
